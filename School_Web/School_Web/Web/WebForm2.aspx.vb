ede7df8bf69ae326657fc1f3db8f104a9338c7d7
wzcb2009 <wzcb2009@163.com>
2015-10-19 08:13:47 +0800

    20151019

---
+++ School_Web/School_Web/Web/WebForm2.aspx.vb
@@ -0,0 +1,9 @@
+﻿Public Class WebForm2
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Response.Write(Request.Form("editor"))
+    End Sub
+
+
+End Class
